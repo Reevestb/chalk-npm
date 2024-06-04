03faa52a91c50705fdd0490aed9b5ac487640ecf
Theo Reeves <theo_25@icloud.com>
2024-06-04 15:00:57 +0100

    trying different colours, no pink or rainbow :(

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import chalk from "chalk";
 console.log(chalk.blue("Hello World!!"));
 console.log(chalk.yellow("Theo is testing chalk out :)"));
+console.log(chalk.red("Does rainbow make my text red"));
